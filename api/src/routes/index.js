e30e3d5216b0850c00f4f02f79b2eeb7233f5721
unknown <pedrocastillo24.pc@gmail.com>
2022-05-25 20:49:55 -0300

    avance

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -57,7 +57,7 @@ router.post('/dog', async (req, res, next) => {
 
 //Filtrado por nombre------------------------------------------------------------
 
-//Me traigo todas las razas de perros-----------------------------------------
+//Me traigo todas las razas de perros y tambien filtro por nombre-----------------------------------------
 
 router.get('/home', async(req, res, next) => {
 
@@ -90,7 +90,8 @@ router.get('/home', async(req, res, next) => {
     const {name} = req.query;
     
     if(name){
-        const nameDog= name[0].toUpperCase() + name.substring(1);
+        const namelower = name.toLowerCase();
+        const nameDog= namelower[0].toUpperCase() + namelower.substring(1);
 
         let filtrado = razas.filter(e => {
 
@@ -111,6 +112,85 @@ router.get('/home', async(req, res, next) => {
         next(error)
     }
 });
+
+//Temperamentos-------------------------------------------------------------
+
+let tempBD = async () => {
+
+    try {
+
+        let dogs = (await axios(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)).data;
+        let temps = [];
+        let aux = '';
+        let filtrado = [];
+
+        for(i=0; i < dogs.length; i++){
+
+            if(dogs[i].temperament){
+
+                let arr = {
+                    temperament: dogs[i].temperament
+                }
+                aux = arr.temperament.split(', ')
+                temps.push(aux)
+            }     
+        }
+
+        temps.forEach(e => {
+                e.forEach(t => {
+                    filtrado.push(t)
+                })     
+            })
+        let filtradoEnd = [...new Set(filtrado)];
+
+         filtradoEnd.forEach(async e => {
+
+          await  Temperament.create({
+    
+                tempName:e
+            })
+            
+        })
+        
+    } catch (error) {
+        next(error)   
+    }
+}
+
+tempBD()
+
+router.get('/home/temperament', async (req,res,next) => {
+    
+try {
+
+        let temp = await Temperament.findAll({
+            attributes:['ID','tempName'],
+            order: [['tempName', 'ASC']]
+        });
+        res.status(200).json(temp)
+
+} catch (error) {
+    next(error)
+}
+
+})
+
+//Filtrado por ID-----------------------------------------------------------
+
+router.get('/home/:id',async (req,res,next) => {
+
+    try {
+        const {id} = req.params;
+console.log(id)
+        let dogy = await Dog.findByPk(id);
+console.log(dogy)
+        res.status(200).json(dogy)
+
+} catch (error) {
+    next(error)
+}
+
+})
 
 
 module.exports = router;
